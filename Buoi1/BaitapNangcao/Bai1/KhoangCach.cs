d2eb2ca2b428b810562a4c57f4d41479874fcea9
Nguyễn Hoàng ấn <bodowq@gmail.com>
2026-09-13 19:46:26 +0700

    Tính khoảng cách 2 điểm trên X y

---
+++ Buoi1/BaitapNangcao/Bai1/KhoangCach.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Bai1;
+
+class KhoangClass
+{
+    static void Main(String[] args)
+    {
+        Console.Write("Nhap vao toa do x diem A: ");
+        double Xa = double.Parse(Console.ReadLine());
+        Console.Write("Nhap vao toa do y diem A: ");
+        double Ya = double.Parse(Console.ReadLine());
+        Console.Write("Nhap vao toa do x diem B: ");
+        double Xb = double.Parse(Console.ReadLine());
+
+        Console.Write("Nhap vao toa do y diem B: ");
+        double Yb = double.Parse(Console.ReadLine());
+        double khoangCach = Math.Sqrt(Math.Pow(Xb - Xa,2)  + Math.Pow(Yb - Ya,2));
+        Console.WriteLine("Khoang cach giua hai diem A va B la: {0}", khoangCach);
+    }
+}
