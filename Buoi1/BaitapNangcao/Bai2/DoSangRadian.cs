d891cb83e461ed9837e95a7f4355f506d0190fd9
Nguyễn Hoàng ấn <bodowq@gmail.com>
2026-09-13 20:35:34 +0700

    Độ sang radian để số thập phân

---
+++ Buoi1/BaitapNangcao/Bai2/DoSangRadian.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class DoSangRadian
+{
+    static void Main(String[] args)
+    {
+        Console.Write("Nhap vao do: ");
+        double doGoc = double.Parse(Console.ReadLine());
+        double radian = (double)(doGoc * Math.PI / 180);
+        Console.WriteLine("{0} do ung voi {1} radian", doGoc, radian);
+    }
+}
